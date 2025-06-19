83071d0dcb9a204903608cba0424c130c06197f7
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-06-19 18:46:00 +0800

    added contacts migration

--- database/migrations/2025_06_19_103314_create_contacts_table.php
+++ database/migrations/2025_06_19_103314_create_contacts_table.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
@@ -19,8 +22,11 @@ class CreateContactsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('contacts');
     }
-}
+};
