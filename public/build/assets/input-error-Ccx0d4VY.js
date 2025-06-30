430db4f77ce64427c433c546677fa293bb2e695d
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-06-30 19:42:47 +0800

    head

---
+++ public/build/assets/input-error-Ccx0d4VY.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Dg84d6fb.js";import{c as o}from"./button-CCMnkL0Z.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
