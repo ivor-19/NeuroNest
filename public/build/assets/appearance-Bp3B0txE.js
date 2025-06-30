af38f97c5e79682fd9ce1c7b486f47308a963766
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-06-30 18:31:39 +0800

    build

---
+++ public/build/assets/appearance-Bp3B0txE.js
@@ -0,0 +1,16 @@
+import{u as y,j as e,K as g,L as r}from"./app-BsLY6kB6.js";import{c as n}from"./button-CcI2b2NU.js";import{c as s}from"./createLucideIcon-CmAh11V1.js";import{S as i,H as o}from"./layout-DkZ4Syse.js";import{A as k}from"./app-layout-DJU5nzQ-.js";import{H as j}from"./header-layout-liuNmkC1.js";import"./separator-DJEBSVfi.js";import"./index-B-Uu_4Og.js";import"./user-menu-content-QMLj2xa5.js";import"./x-BYyvnJYh.js";import"./index-CC9Haw-T.js";import"./app-logo-icon-C7QEiCKm.js";import"./users-BWmvxBYP.js";import"./graduation-cap-BanZ23NO.js";import"./mail-IgDJE4aw.js";import"./index-DolsK6my.js";import"./menu-0Hxbg_V3.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],f=s("Monitor",b);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],A=s("Moon",v);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],M=s("Sun",N);function c({className:t="",...l}){const{appearance:m,updateAppearance:d}=y(),x=[{value:"light",icon:M,label:"Light"},{value:"dark",icon:A,label:"Dark"},{value:"system",icon:f,label:"System"}];return e.jsx("div",{className:n("inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800",t),...l,children:x.map(({value:a,icon:h,label:u})=>e.jsxs("button",{onClick:()=>d(a),className:n("flex items-center rounded-md px-3.5 py-1.5 transition-colors",m===a?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"),children:[e.jsx(h,{className:"-ml-1 h-4 w-4"}),e.jsx("span",{className:"ml-1.5 text-sm",children:u})]},a))})}const p=[{title:"Appearance settings",href:"/settings/appearance"}];function Z(){const{auth:t}=g().props;return e.jsx(e.Fragment,{children:t.user.role==="admin"?e.jsxs(k,{breadcrumbs:p,children:[e.jsx(r,{title:"Appearance settings"}),e.jsx(i,{children:e.jsxs("div",{className:"space-y-6",children:[e.jsx(o,{title:"Appearance settings",description:"Update your account's appearance settings"}),e.jsx(c,{})]})})]}):e.jsxs(j,{breadcrumbs:p,children:[e.jsx(r,{title:"Appearance settings"}),e.jsx("div",{className:"mt-16"}),e.jsx(i,{children:e.jsxs("div",{className:"space-y-6",children:[e.jsx(o,{title:"Appearance settings",description:"Update your account's appearance settings"}),e.jsx(c,{})]})})]})})}export{Z as default};
